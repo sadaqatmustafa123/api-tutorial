95b7b5685590b8bb5872d24b1d7569e85831722f
sadaqatmustafa123 <sadaqatmustafa12345@gmail.com>
2024-03-03 01:33:08 +0500

    add post api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:apitutorial/Models/PostsModel.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +12,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postList = [];
+  Future<List<PostsModel>> getPostApi () async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if(response.statusCode == 200) {
+      for(Map i in data){
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    }
+    return postList;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +31,25 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('API Course'),
         centerTitle: true,
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: getPostApi(),
+            builder: (context, snapshot){
+              if (!snapshot.hasData) {
+                return const Text('Loading');
+
+              }else {
+                return ListView.builder(
+                    itemCount: postList.length,
+                    itemBuilder: (context, index) {
+                  return Text(index.toString());
+                });
+
+              }
+            },
+          )
+        ],
       ),
     );
   }
